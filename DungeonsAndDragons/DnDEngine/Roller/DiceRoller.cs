812880bc3e54729352b32857b35eebdc4a732989
ben9322 <benjamin.yen@gmail.com>
2016-01-26 20:19:52 -0600

    Added changes to DiceRoller

--- DungeonsAndDragons/DnDEngine/Roller/DiceRoller.cs
+++ DungeonsAndDragons/DnDEngine/Roller/DiceRoller.cs
@@ -4,50 +4,50 @@ namespace DnDEngine.Roller
 {
     public static class DiceRoller
     {
-        public static int RollD100()
+        public static int RollD100(int numberOfDice)
         {
-            return new Random().Next(1, 100);
+            return RollDice(numberOfDice, 100);
         }
 
-        public static int RollD20()
+        public static int RollD20(int numberOfDice)
         {
-            return new Random().Next(1, 20);
+            return RollDice(numberOfDice, 20);
         }
 
-        public static int RollD12()
+        public static int RollD12(int numberOfDice)
         {
-            return new Random().Next(1, 12);
+            return RollDice(numberOfDice, 12);
         }
 
-        public static int RollD10()
+        public static int RollD10(int numberOfDice)
         {
-            return new Random().Next(1, 10);
+            return RollDice(numberOfDice, 10);
         }
 
-        public static int RollD8()
+        public static int RollD8(int numberOfDice)
         {
-            return new Random().Next(1, 8);
+            return RollDice(numberOfDice, 8);
         }
 
-        public static int RollD6()
+        public static int RollD6(int numberOfDice)
         {
-            return new Random().Next(1, 6);
+            return RollDice(numberOfDice, 6);
         }
 
-        public static int RollD4()
+        public static int RollD4(int numberOfDice)
         {
-            return new Random().Next(1, 4);
+            return RollDice(numberOfDice, 4);
         }
 
         // the no good fun killer lazy version of rolling dice
-        public static int RollDice(int NumberOfSides, int NumberOfDice)
+        public static int RollDice(int numberOfSides, int numberOfDice)
         {
-            int RandomNumber = 0;
-            for (int i = 1; i <= NumberOfDice; i++)
+            int randomNumber = 0;
+            for (int i = 1; i <= numberOfDice; i++)
             {
-                RandomNumber += new Random().Next(1, NumberOfSides);
+                randomNumber += new Random().Next(1, numberOfSides);
             }
-            return RandomNumber;
+            return randomNumber;
         }
     }
 }
