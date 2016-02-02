7344d9b8b71898dc3c154bbce2870c0e90594414
Endurance Idehen <eidehen@boongroup.com>
2016-02-02 08:49:25 -0600

    dnd engine with roundhouse

---
+++ DungeonsAndDragons/DndEngine.Migration/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndEngine.Migration
+{
+    public class Class1
+    {
+        public void AddingFunction(int inParameter1, int inParameter2, out int outParameter)
+        {
+            int something = inParameter1 + inParameter2;
+        }
+
+        public void CallingFunction()
+        {
+            int thing1 = 10;
+            int thing2 = 20;
+            int result;
+
+            AddingFunction(thing1, thing2, out result);
+           
+        }
+    }
+}
